d56f249b8ff0a9750cf2fe58252703864f3abba9
lutfiqaraman <lutfiqaraman@gmail.com>
2020-03-09 00:12:51 +0200

    Add project files.

---
+++ Find.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestWord
+{
+    public class Find
+    {
+        public List<string> FindLongestWord(string text)
+        {
+            List<string> result = new List<string>();
+            int lengthoflongestWord = 0;
+            List<string> arrayText = new List<string>();
+
+            //Remove punctuation from the text
+            string rowText = new string((from c in text
+                                         where char.IsWhiteSpace(c) || char.IsLetterOrDigit(c)
+                                         select c).ToArray());
+
+            arrayText = rowText.Split(' ').ToList();
+            lengthoflongestWord = arrayText.Max(x => x.Length);
+
+            for (int i = 0; i < arrayText.Count - 1; i++)
+            {
+                if (arrayText[i].Length == lengthoflongestWord)
+                    result.Add(arrayText[i]);
+                    
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string text =
+                "The longest word in any of the major English language dictionaries is 9pneumonoultramicroscopicsilicovolcanoconiosis, " +
+                "a word that refers to a lung disease contracted from the inhalation of very fine silica particles, " +
+                "specifically from a volcano; medically, it is the same as silicosis.";
+
+            Find Search = new Find();
+
+            List<string> result = Search.FindLongestWord(text);
+
+            Console.WriteLine("The longest word(s) in the text");
+
+            foreach (var item in result)
+                Console.WriteLine(item);
+
+            Console.ReadKey();
+        }
+    }
+}
